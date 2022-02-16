40c8cdfe2f320cc8206097c4add311499b3195b2
Delaiah <delaiahiyob@gmail.com>
2022-02-15 18:13:57 -0800

    attempted to make buttons clickable

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Delaiah" // HINT: Replace this with your own name!
+// make sure buttons are clickable
+//make functions for buttons
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -10,18 +12,39 @@ let sugar = 0 // Sugar Sprinkle
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
+
 // selecting the element with an id of add-gb
 const gbPlusBtn = document.querySelector('#add-gb')
+console.log(gbPlusBtn)
+//minus-gb
+const gbMinusBtn = document.querySelector('#minus-gb')
+console.log(gbMinusBtn)
 
 // Code to update name display
-credit.textContent = `Created by ${yourName}`
+credit.textContent = `Created by ${'Delaiah'}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
+const totalNum = document.querySelector('gb')
+console.log(totalNum)
+gbPlusBtn.addEventListener('click', function(e){
+    gb = gb + 1;
+    console.log(gb);
+}); 
+
 // HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
 
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
+
 
 // TODO: Hook up event listeners for the rest of the buttons
+const ccPlusBtn = document.querySelector('#add-cc')
+console.log(ccPlusBtn)
+
+const ccMinusBtn = document.querySelector('#minus-cc')
+console.log(ccMinusBtn)
+
+const sugarPlusBtn = document.querySelector('#add-sugar')
+console.log(sugarPlusBtn)
+
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+console.log(sugarMinusBtn)
