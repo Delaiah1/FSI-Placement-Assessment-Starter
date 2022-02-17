6a9852f1385dcbcf4a9775ba96823b2378267487
Delaiah <delaiahiyob@gmail.com>
2022-02-17 15:04:49 -0800

    added functionality to gbPlsBtn

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+
 // First, tell us your name
 let yourName = "Delaiah" // HINT: Replace this with your own name!
-// make sure buttons are clickable
-//make functions for buttons
+//buttons = clickable
+//make functions for them
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -15,8 +14,10 @@ const credit = document.querySelector('#credit')
 
 // selecting the element with an id of add-gb
 const gbPlusBtn = document.querySelector('#add-gb')
-console.log(gbPlusBtn)
+const gbQty = document.querySelector('#qty-gb')
+
 //minus-gb
+
 const gbMinusBtn = document.querySelector('#minus-gb')
 console.log(gbMinusBtn)
 
@@ -24,14 +25,12 @@ console.log(gbMinusBtn)
 credit.textContent = `Created by ${'Delaiah'}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-const totalNum = document.querySelector('gb')
-console.log(totalNum)
-gbPlusBtn.addEventListener('click', function(e){
-    gb = gb + 1;
-    console.log(gb);
-}); 
 
-// HINT: You can delete this console.log after you no longer need it!
+gbPlusBtn.addEventListener('click', function(){
+        gb = gb + 1
+        console.log(gb)
+        gbQty.textContent = gb
+    })
 
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 
